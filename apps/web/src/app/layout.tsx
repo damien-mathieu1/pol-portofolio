3bd57f6d1c662994ca5be360efe2f0d079dac3a7
Damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2025-06-04 23:44:01 +0200

    change images

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,11 +3,8 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "@/styles/globals.css";
 
 import Script from "next/script";
-import { metadata as meta } from "@/app/config";
 import Loader from "@/app/loader";
 import Providers from "@/app/providers";
-import { env } from "@/env";
-import { createMetadata } from "@/lib/metadata";
 
 import { Toaster } from "@repo/ui/sonner";
 
@@ -15,8 +12,8 @@ import { Toaster } from "@repo/ui/sonner";
 const bricolage_grotesque = Bricolage_Grotesque({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
-  description: "Portfolio website",
+  title: "Paul Rouviere",
+  description: "Paul Rouviere's Portfolio",
 };
 
 export default function RootLayout({
--- apps/web/src/components/sections/skills/config.ts
+++ apps/web/src/components/sections/skills/config.ts
@@ -2,35 +2,35 @@ import type { Skill } from "@/types/skill";
 
 const skills: Skill[] = [
   {
-    name: "🎥 Lighting & Look Development",
+    name: "Lighting & Look Development",
     // todo: use proper thumbnail pics
-    thumbnail: "/images/skills/web-development.jpg",
+    thumbnail: "/images/skills/lighting.jpg",
     description: `Lighting and lookdev are at the heart of my creative process. I focus on building cinematic lighting setups that enhance mood and realism, while developing shaders and materials that bring assets to life. I aim to strike a balance between artistic intent and physical plausibility, using tools like V-Ray and Maya.`,
   },
   {
-    name: "🧑‍💻 Compositing",
-    thumbnail: "/images/skills/ui-ux-design.jpg",
-    description: `Using Nuke, I bring together 3D renders and 2D elements to polish and enhance final shots. From AOV management to grading and integration, compositing is where I refine details and control the final image—bridging technical renders with creative intent.`,
+    name: "Compositing",
+    thumbnail: "/images/skills/compositing.jpg",
+    description: `Using Nuke, I bring together 3D renders and 2D elements to polish and enhance final shots. From AOV management to grading and integration, compositing is where I refine details and control the final image bridging technical renders with creative intent.`,
   },
   {
-    name: "🌍 Environment Creation",
-    thumbnail: "/images/skills/database-management.jpg",
+    name: "Environment Creation",
+    thumbnail: "/images/skills/environment-creation.jpg",
     description: `Whether it's natural landscapes or stylized worlds, I use tools like Gaea and SpeedTree to build environments that support storytelling. I pay close attention to scale, depth, and atmosphere to ensure believable spaces that complement the subject.`,
   },
   {
-    name: "📽️ Full CG Workflow",
-    thumbnail: "/images/skills/mobile-development.jpg",
-    description: `From layout to animation, lighting, rendering, and editing—I’m used to managing every stage of a 3D production pipeline. This versatility allows me to stay flexible and take ownership of entire projects, especially in personal work.`,
+    name: "Full CG Workflow",
+    thumbnail: "/images/skills/full-cg-workflow.jpg",
+    description: `From layout to animation, lighting, rendering, and editing I’m used to managing every stage of a 3D production pipeline. This versatility allows me to stay flexible and take ownership of entire projects, especially in personal work.`,
   },
   {
-    name: "🎬 Editing & Post-production",
-    thumbnail: "/images/skills/mobile-development.jpg",
+    name: "Editing & Post-production",
+    thumbnail: "/images/skills/editing.jpg",
     description: `I use Premiere Pro to shape narrative flow, pacing, and rhythm in my videos. Editing is key to delivering impact and emotional tone, especially in commercial-style projects where timing and transitions define the piece.`,
   },
   {
-    name: "🚘 Product & Automotive Visualization",
-    thumbnail: "/images/skills/mobile-development.jpg",
-    description: `My recent work focuses on creating high-end visualizations for cars and luxury products. The goal is to make them feel grounded in reality—through detailed lighting, careful reflections, and premium staging.`,
+    name: "Product & Automotive Visualization",
+    thumbnail: "/images/skills/product-visualization.jpg",
+    description: `My recent work focuses on creating high-end visualizations for cars and luxury products. The goal is to make them feel grounded in reality through detailed lighting, careful reflections, and premium staging.`,
   }
 ];
 
