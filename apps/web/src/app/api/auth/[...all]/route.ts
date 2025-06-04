21616e8de83bd46dd3f439843ed24fc694d9abf9
Damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2025-06-04 23:27:07 +0200

    fix

--- apps/web/src/app/api/auth/[...all]/route.ts
+++
@@ -1,7 +0,0 @@
-// Removed auth API route to disable authentication
-export const POST = async () => {
-  return new Response(null, { status: 404 });
-};
-export const GET = async () => {
-  return new Response(null, { status: 404 });
-};
